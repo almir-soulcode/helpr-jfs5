c3d815a55b4f4cca89a09362fd6fa894efd6aa3a
jose-almir <jr.cod.dev@gmail.com>
2022-10-31 10:35:50 -0300

    Estrutura base do projeto

---
+++ src/main/java/org/soulcodeacademy/helpr/HelprApplication.java
@@ -0,0 +1,13 @@
+package org.soulcodeacademy.helpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelprApplication.class, args);
+	}
+
+}
