c7c188105fe3ada0db8795c217c88157f6714771
jose-almir <jr.cod.dev@gmail.com>
2022-11-01 11:12:46 -0300

    Classe de configuração de desenvolvedor

---
+++ src/main/java/org/soulcodeacademy/helpr/config/DevConfig.java
@@ -0,0 +1,31 @@
+package org.soulcodeacademy.helpr.config;
+
+import org.soulcodeacademy.helpr.services.PopulateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+
+// indica que esta classe será gerenciada pelo Spring
+// e que ela é uma classe de configuração
+@Configuration
+@Profile("dev") // será instanciada apenas se o perfil ativo for "dev"
+public class DevConfig {
+    // Esta classe irá chamar o PopulateService caso o perfil ativo seja de desenvolvimento
+    @Autowired
+    private PopulateService populateService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}") // vai acessar a propriedade
+    private String ddlAuto;
+
+    @PostConstruct // faz com que o método execute após a instância ser criada
+    public void init() {
+        // se o modo for create, insere os dados fictícios
+        if(this.ddlAuto.equals("create")) {
+            // inserir os dados novos caso a tabela tenha sido dropada
+            this.populateService.populate();
+        }
+    }
+}
