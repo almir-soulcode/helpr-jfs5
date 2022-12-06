287c9f0353ff4a213cfba7f48c595c1e792fd997
gabrielanataline <gabiesnataline@hotmail.com>
2022-12-06 17:45:11 -0300

    Criação de entidade FuturoCliente e FuturoClienteRepository

---
+++ src/main/java/org/soulcodeacademy/helpr/domain/FuturoCliente.java
@@ -0,0 +1,63 @@
+package org.soulcodeacademy.helpr.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class FuturoCliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+
+    @Column(nullable = false, length = 150)
+    protected String nome;
+
+    @Column(nullable = false, unique = true, length = 120)
+    protected String email;
+
+    @Column(nullable = false, unique = true, length = 11)
+    protected String cpf;
+
+    public FuturoCliente (){}
+
+    public FuturoCliente(Integer id, String nome, String email, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ src/main/java/org/soulcodeacademy/helpr/repositories/FuturoClienteRepository.java
@@ -0,0 +1,12 @@
+package org.soulcodeacademy.helpr.repositories;
+import org.soulcodeacademy.helpr.domain.FuturoCliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface FuturoClienteRepository extends JpaRepository <FuturoCliente, Integer> {
+
+    List<FuturoCliente> findByEmail(String email);
+    List<FuturoCliente> findByCpf (String cpf);
+}
