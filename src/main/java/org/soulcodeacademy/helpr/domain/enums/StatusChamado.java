43c3782a990c3932840edbf95ce46754db707a75
ManoellyBernardo <manoellymaria@gmail.com>
2022-12-07 20:23:49 -0300

    Adição de status ARQUIVADO em chamados.

--- src/main/java/org/soulcodeacademy/helpr/domain/enums/StatusChamado.java
+++ src/main/java/org/soulcodeacademy/helpr/domain/enums/StatusChamado.java
@@ -3,5 +3,6 @@ package org.soulcodeacademy.helpr.domain.enums;
 public enum StatusChamado {
     RECEBIDO, // Quando o chamado é criado
     ATRIBUIDO, // Quando um funcionário está resolvendo o chamado
-    CONCLUIDO // Quando o chamado foi resolvido
+    CONCLUIDO, // Quando o chamado foi resolvido
+    ARQUIVADO
 }
