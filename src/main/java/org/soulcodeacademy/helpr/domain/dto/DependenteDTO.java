74d6ff6a0321b439aaf46eb1fe5517f1d6abca03
DKFAEL <rafaelnunesemp@hotmail.com>
2022-12-08 10:48:34 -0300

    dependenteService

--- src/main/java/org/soulcodeacademy/helpr/domain/dto/DependenteDTO.java
+++ src/main/java/org/soulcodeacademy/helpr/domain/dto/DependenteDTO.java
@@ -11,6 +11,7 @@ import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 public class DependenteDTO {
 
@@ -22,12 +23,51 @@ public class DependenteDTO {
     private String cpf;
 
     @NotNull(message = "Insira uma data de nascimento válida")
-    private Integer dataDeNascimento;
+    private LocalDate dataDeNascimento;
 
     @NotBlank(message = "Escolaridade é obrigatório")
     private String escolaridade;
 
-    @NotNull(message = "Insira um IdResponsavel válido")
-    private String idDependente ;
+    @NotNull
+    private  Integer idFuncionario;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getEscolaridade() {
+        return escolaridade;
+    }
+
+    public void setEscolaridade(String escolaridade) {
+        this.escolaridade = escolaridade;
+    }
+
+    public Integer getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(Integer idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
 }
---
+++ src/main/java/org/soulcodeacademy/helpr/services/DependenteService.java
@@ -0,0 +1,84 @@
+package org.soulcodeacademy.helpr.services;
+
+import org.soulcodeacademy.helpr.domain.Dependente;
+import org.soulcodeacademy.helpr.domain.Funcionario;
+import org.soulcodeacademy.helpr.domain.dto.DependenteDTO;
+import org.soulcodeacademy.helpr.repositories.DependenteRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Service
+public class DependenteService {
+
+    public DependenteRepository dependenteRepository;
+
+    public  FuncionarioService funcionarioService ;
+
+    public List<Dependente> listar() {
+        return  this.dependenteRepository.findAll();
+    }
+
+    public Dependente getDependente(Integer idDependendte){
+        return this.dependenteRepository.findById(idDependendte)
+                .orElseThrow(() -> new RuntimeException("Dependente não encontrado!"));
+    }
+
+    public  Dependente salvar (DependenteDTO dto){
+        Funcionario funcionario = this.funcionarioService.getFuncionario(dto.getIdFuncionario());
+
+        long years = ChronoUnit.YEARS.between(dto.getDataDeNascimento(), LocalDate.now());
+
+        if (years < 18){
+            Dependente dependente = new Dependente(null, dto.getNome(), dto.getCpf(), dto.getDataDeNascimento(), dto.getEscolaridade(), funcionario);
+
+            return  this.dependenteRepository.save(dependente);
+        } else  {
+            throw new RuntimeException(" Acima de  18 anos");
+        }
+    }
+
+    public Dependente atualizar( Integer idDependente, DependenteDTO dto) {
+        Dependente dependente = this.getDependente(idDependente);
+
+        if (dependente.getFuncionario() == this.funcionarioService.getFuncionario(dto.getIdFuncionario())){
+
+            dependente.setNome(dto.getNome());
+            dependente.setDataDeNascimento(dto.getDataDeNascimento());
+            dependente.setCpf(dto.getCpf());
+            dependente.setEscolaridade(dto.getEscolaridade());
+
+
+            return this.dependenteRepository.save(dependente);
+        } else {
+            throw new RuntimeException("Não foi possivel alterar  o responsável");
+        }
+     }
+
+     public void  deletar(Integer idDependente) {
+        Dependente dependente = this.getDependente(idDependente);
+        this.dependenteRepository.delete(dependente);
+     }
+
+        public Dependente filtrarCpf(String cpf) {
+        return (Dependente) this.dependenteRepository.findByCpf(cpf);
+        }
+
+    public List<Dependente> filtrarData(String valor1, String valor2){
+
+        LocalDate v1 = LocalDate.parse(valor1);
+        LocalDate v2 = LocalDate.parse(valor2);
+        return this.dependenteRepository.findByDataEntreFaixas(v1, v2);
+    }
+
+    public List<Dependente> filtrarResponsavel(Integer idFuncionario){
+
+        Funcionario funcionario = this.funcionarioService.getFuncionario(idFuncionario);
+
+        return this.dependenteRepository.findByFuncionario(funcionario);
+    }
+    }
+
+
